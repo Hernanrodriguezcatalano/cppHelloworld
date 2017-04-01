47fcda4d13aa958c749ac55d344c264864a10839
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2017-04-01 18:39:38 -0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+/*Probar el compilador con un programa hello.cpp que envíe a cout la línea
+Hello, World! o similar.
+Hello.cpp
+Hernán Rodríguez Catalano
+01/04/2017
+
+*/
+#include <iostream>
+int main()
+{
+	std:: cout << "Hello World!";
+
+}
+
